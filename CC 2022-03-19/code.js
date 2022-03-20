02ba8fbb96cc6a7047ec296f9cef8f8a74bf5649
Aimee Sharp <sharpaim23@gmail.com>
2022-03-20 11:50:23 -0500

    Add coding challenge for 2022-03-20.

--- CC 2022-03-19/code.js
+++ CC 2022-03-19/code.js
@@ -1,1 +1,13 @@
 //We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+let stringToNumber = function(str){
+  return str = str.toString()
+  
+  }
+
+  console.log(stringToNumber(8));
+
+
+// Create a function that transforms a string into a number
+//  Can use  .toString()
+// Return number to string
