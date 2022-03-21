48b25fcc41f58ea3b130a67943133b36c8c251b3
Aimee Sharp <sharpaim23@gmail.com>
2022-03-21 11:57:32 -0500

    Add coding challenge for 2022-03-21.

---
+++ CC 2022-03-21/code.js
@@ -0,0 +1,8 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present //in the array (true means present).
+// for example,
+// [true,true,true,false,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true]
+
+
+function countSheeps(arrayOfSheep){
+  return arrayOfSheep.filter(item => item === true).length
+}
