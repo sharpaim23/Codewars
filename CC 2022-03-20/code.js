4cb947fc6f157963f9f0a96bc868781ac5155940
Aimee Sharp <sharpaim23@gmail.com>
2022-04-12 11:51:43 -0500

    "Saved file"

---
+++ CC 2022-03-20/code.js
@@ -0,0 +1,10 @@
+//Make a simple function called greet that returns the most-famous 'hello world!'.
+
+function greet(){
+  return 'hello world!'
+  }
+
+  console.log('hello world!');
+//Create a function greet
+// Return hello world!
+// Call 'hello world!'
