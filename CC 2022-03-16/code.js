bb1447327128cc202a86bbb5b0f84d311dfab918
Aimee Sharp <sharpaim23@gmail.com>
2022-04-12 09:47:45 -0500

    Add code for coding challenge CC 2022-03-16/code.js

--- CC 2022-03-16/code.js
+++ CC 2022-03-16/code.js
@@ -1,3 +1,14 @@
 //Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
-
+function fakeBin(str){
+  var newStr = "";
+  for(var i=0;i<str.length;i++){
+    if(Number(str[i])>=5){
+      newStr += "1"
+    }
+    else{
+      newStr += "0";
+    }
+  }
+  return newStr;
+}
