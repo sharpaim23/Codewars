ab92785f5cd231c94376cd8a9fd7022f23381423
Aimee Sharp <sharpaim23@gmail.com>
2022-05-05 14:40:29 -0500

    Add coding challenges code.js2,3,4,5 for 2022-05-05

---
+++ CC 2022-05-05/code4.js
@@ -0,0 +1,28 @@
+/*Challenge:
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+Addendum:
+Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
+
+http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well */
+
+//SAMPLE TESTS:
+/*describe("Example test cases", function() {
+  Test.assertSimilar(flattenAndSort([]), []);
+  Test.assertSimilar(flattenAndSort([[], []]), []);
+  Test.assertSimilar(flattenAndSort([[], [1]]), [1]);
+  Test.assertSimilar(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  Test.assertSimilar(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
+}); */
+
+//SOLUTION:
+"use strict";
+
+function flattenAndSort(array) {
+  let array2 = ([].concat(...array).sort((a,b) => a-b))
+  return array2
+}
+
