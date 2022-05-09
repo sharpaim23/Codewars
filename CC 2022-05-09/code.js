f3e5802af3db7bdcddfbeadaf5f4272c341c490f
Aimee Sharp <sharpaim23@gmail.com>
2022-05-09 04:36:20 -0500

    Add coding challenges code.js2,3,4,5 for 2022-05-09

---
+++ CC 2022-05-09/code.js
@@ -0,0 +1,24 @@
+/*You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.*/
+
+//SAMPLE TESTS:
+/* const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(otherAngle(30, 60), 90);
+    assert.strictEqual(otherAngle(60, 60), 60);
+    assert.strictEqual(otherAngle(43, 78), 59);
+    assert.strictEqual(otherAngle(10, 20), 150);
+  });
+});*/
+
+//SOLUTION:
+function otherAngle(a, b) {
+  return 180 - (a+b)
+}
