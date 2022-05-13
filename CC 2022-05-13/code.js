3dd920be09269ba457061c526e56bb30fef89d93
Aimee Sharp <sharpaim23@gmail.com>
2022-05-13 04:08:24 -0500

    Add a coding challenege for 2022-05-13

---
+++ CC 2022-05-13/code.js
@@ -0,0 +1,50 @@
+/*The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth" */
+
+//SAMPLE TEST:
+/* describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(getPlanetName(2), 'Venus');
+Test.assertEquals(getPlanetName(5), 'Jupiter');
+Test.assertEquals(getPlanetName(3), 'Earth');
+});
+});*/
+
+//SOLUTION:
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+      break;
+      default:
+        break;
+  }
+  
+  return name
+}
+console.log(getPlanetName(3));
