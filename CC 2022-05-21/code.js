dadcb3f38fb181a41ede3e6f9e935626f1cd4bb6
Aimee Sharp <sharpaim23@gmail.com>
2022-05-21 05:38:07 -0500

    Add a coding challenege for 2022-05-21

---
+++ CC 2022-05-21/code.js
@@ -0,0 +1,42 @@
+/*Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Calculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).*/
+
+//SAMPLE TESTS:
+/*describe("Sample tests", function(){
+  it("Testing for dad's age: 36 and son's age: 7", function(){
+    Test.assertEquals(twiceAsOld(36,7) , 22);
+  });  
+  it("Testing for dad's age: 55 and son's age: 30", function(){
+    Test.assertEquals(twiceAsOld(55,30) , 5);
+  });
+  it("Testing for dad's age: 42 and son's age: 21", function(){
+    Test.assertEquals(twiceAsOld(42,21) , 0);
+  });
+  it("Testing for dad's age: 22 and son's age: 1", function(){
+    Test.assertEquals(twiceAsOld(22,1) , 20);
+  });
+  it("Testing for dad's age: 29 and son's age: 0", function(){
+    Test.assertEquals(twiceAsOld(29,0) , 29);
+  });
+});*/
+
+//P: Will it ever be a string, always positive integer, will another child be ever be included
+//R: How many years age the dad's age was twice as old as his son's age
+
+//SOLUTION:
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  //P:dadyearsold sub 2
+  //multiply sonsyearsold by 2
+  //show work
+  return Math.abs(dadYearsOld - 2 * sonYearsOld)
+}
+
+
+
+//E:
+twiceAsOld(36,7) , 22
+twiceAsOld(42,21) , 0
+twiceAsOld(22,1) , 20
