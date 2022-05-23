438d2f808560dbfddc62bb857b049a25cadddc5f
Aimee Sharp <sharpaim23@gmail.com>
2022-05-23 06:52:48 -0500

    Fixed the title

---
+++ CC 2022-05-22/code.js
@@ -0,0 +1,40 @@
+/*Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year. */
+
+
+//SAMPLE TESTS:
+/*describe("total goals", function(){
+    it ("should equal the total", function () {
+      Test.assertEquals(totalGoals, 58)
+    })
+})*/
+
+//P:Are there to be any other submissions that are not accounted for already, will they be from a different league, can goals be taken away
+//R: The total number of goals across all leagues from Messi
+
+
+//SOLUTION:
+//P: Assign values to all the variables
+//And put the total in totalGoals variable
+//Show work
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = 58
+
+//E: It's just about assigning variables a value
