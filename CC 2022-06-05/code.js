f3fb9adeae2dbd5b2d840742712e1a7d440dc87b
Aimee Sharp <sharpaim23@gmail.com>
2022-06-05 04:00:08 -0500

    Added coding challenge for 2022-06-05

---
+++ CC 2022-06-05/code.js
@@ -0,0 +1,73 @@
+/*Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+The Database
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+Possible invalid inputs include:
+
+IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+IP_ADDRESS_NOT_FOUND - ip address not in the database
+IP_ADDRESS_REQUIRED - no ip address was supplied */
+
+//SAMPLE TESTS:
+/*describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+Test.assertEquals(greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again.");
+Test.assertEquals(greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+  });
+});*/
+
+//P:Can the object be empty, Can this possibly be turned into arrays, can the arrays be empty 
+//R:The specific welcome according to the ip address
+
+//SOLUTION:
+function greet(language) {
+  //P:Create an object of languages 
+  //return langue default or the language the ip results show
+  //show work
+  const base = {
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+  }
+	return base[language] || base.english
+}
+
+
+//E:
+greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again."
+greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again."
+greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again."
