404fc5682e4012930b583977f04c6e24e8a8d246
Aimee Sharp <sharpaim23@gmail.com>
2022-07-09 06:55:45 -0500

    Added coding challenge for 2022-07-09

---
+++ CC 2022-07-09/code.js
@@ -0,0 +1,57 @@
+/*If you've completed this kata already and want a bigger challenge, here's the 3D version
+
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
++---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
++---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+Examples (input -> output:
+* 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+* 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]*/
+
+//SAMPLE TESTS:
+/*// Since Node 10, we're using Mocha.
+// You can use `chai` for assertions.
+const chai = require("chai");
+const assert = chai.assert;
+// Uncomment the following line to disable truncating failure messages for deep equals, do:
+// chai.config.truncateThreshold = 0;
+// Since Node 12, we no longer include assertions from our deprecated custom test framework by default.
+// Uncomment the following to use the old assertions:
+// const Test = require("@codewars/test-compat");
+
+describe("Solution", function() {
+  it("fixed tests", function() {
+    assert.deepEqual(flip('R', [3, 2, 1, 2]), [1, 2, 2, 3]);
+    assert.deepEqual(flip('L', [1, 4, 5, 3, 5]), [5, 5, 4, 3, 1]);
+  });
+}); */
+
+//P: Can there been an empty array, or negative integers, or strings
+//R: How many cubes are in the column once the gravity is switched
+
+//SOLUTION:
+//P: If d is equal type/input 
+// then sort a
+// for left to right
+// and top and bottom
+// show work
+const flip=(d, a)=>{
+  return d === 'R' ? a.sort(function(x,y){return x-y}) : a.sort(function(x,y){return y-x})
+}
+
+
+
+//E:
+flip('R', [3, 2, 1, 2]), [1, 2, 2, 3]
+flip('L', [1, 4, 5, 3, 5]), [5, 5, 4, 3, 1]
