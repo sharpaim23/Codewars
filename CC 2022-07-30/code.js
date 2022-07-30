eff590b4bcd4a36ad40e255c677988e7ce2b9257
Aimee Sharp <sharpaim23@gmail.com>
2022-07-30 04:04:29 -0500

    Added coding challenge for 2022-07-30

---
+++ CC 2022-07-30/code.js
@@ -0,0 +1,31 @@
+/*Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)*/
+
+//SAMPLE TESTS:
+/*describe("unusualFive", function(){
+  it("should return 5", function(){
+    assert.strictEqual(unusualFive(), 5);
+  });
+});*/
+
+
+//P: Can strings be allowed, arrays that return a single numbered response, numbers from strings
+//R: A function that always returns 5
+
+//SOLUTION:
+//P: Pick a 5 lettered word
+// Return the length of that word
+// 5 is returned
+// Show work
+function unusualFive() {
+  return 'jumps'.length
+}
+
+
+//E:
+unusualFive(bangs.length), 5
+unusualFive(sings.length), 5
+unusualFive(Diana.length), 5
