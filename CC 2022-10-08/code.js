db709f666af6c87be4cde8b1d302af15f9314f29
Aimee Sharp <sharpaim23@gmail.com>
2022-10-08 05:29:43 -0500

    Adding coding challenge for 2022-10-08

---
+++ CC 2022-10-08/code.js
@@ -0,0 +1,28 @@
+/*Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34 */
+
+//SAMPLE TESTS:
+/*describe("twoDecimalPlaces", function() {
+  it("works for some examples", function() {
+    Test.assertEquals(twoDecimalPlaces(4.659725356), 4.66, "didn't work for 4.659725356");
+    Test.assertEquals(twoDecimalPlaces(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948");
+    Test.assertEquals(twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356");
+  });
+});*/
+
+
+//P: Numbers - rounded decimal places by two spaces, all valid numbers
+//R: Return the number with two decimal places
+
+//SOLUTION:
+//P:
+twoDecimalPlaces = (n) => Number.parseFloat(n).toFixed(2) * 1
+
+
+//E
+twoDecimalPlaces(4.659725356), 4.66, "didn't work for 4.659725356"
+twoDecimalPlaces(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948"
+twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356"
