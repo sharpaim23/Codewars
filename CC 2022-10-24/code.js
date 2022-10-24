2f5d415b7e676b6b3cbebf708735a9239090ab64
Aimee Sharp <sharpaim23@gmail.com>
2022-10-24 03:18:01 -0500

    Added Coding Challenege for 2022-10-24

---
+++ CC 2022-10-24/code.js
@@ -0,0 +1,53 @@
+/*This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.*/
+
+//SAMPLE TESTS:
+/*const Test = require('@codewars/test-compat');
+
+describe("accum",function() {
+it("Basic tests",function() {    
+	Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+	Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+	Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+	Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+	Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+})})*/
+
+
+//Strings -> includes only letters for a..z/A..Z, can be used with any word are a string of characters
+// Return the the param of accum in the way it is provided
+
+//accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+//accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+//accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+
+//SOLUTION:
+function accum(s) {
+  // declare variable for empty string
+  let result = ''
+  // turn string into lower case and split
+  let array = s.toLowerCase().split('')
+  // for loop to turn index[0] into Upper case
+  for (let i = 0; i < array.length; i++){
+    result += array[i].toUpperCase()
+    // 2nd for loop to decrease the number of letters per each character
+    for (let j = i; j > 0; j--){
+      result += array[i]
+    }
+    // if else in include the '-' in between each set of characters
+    if (i < array.length - 1) {
+      result += '-'
+    }
+  }
+  return result
+}
+
+//Solution 2
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
